322fb17efe934fa54721de99d0b92b0fb8ea980f
kosta7bravo-beep <kosta7bravo@gmail.com>
2026-08-05 21:06:44 +0300

    Update football.py

--- football.py
+++ football.py
@@ -1,5 +1,6 @@
 from telegram_bot import send_message
 from match_analyzer import analyze_match
+from match_provider import get_matches
 
 already_sent = False
 
@@ -11,24 +12,21 @@ async def check_football():
 
     already_sent = True
 
-    # Пока тестовый матч.
-    # Позже вместо него будут реальные данные.
-    match = {
-        "league": "Premier League",
-        "home": "Arsenal",
-        "away": "Chelsea",
-        "odd": 1.91,
-        "home_form": 4,
-        "away_form": 3,
-        "goals_avg": 2.9
-    }
+    matches = get_matches()
 
-    result = analyze_match(match)
+    for match in matches:
 
-    if result["score"] >= 80:
-        await send_message(
-            f"🟢 <b>STRONG SIGNAL</b>\n\n"
-            f"⚽ {match['home']} — {match['away']}\n\n"
-            f"📊 Оценка: {result['score']}/100\n\n"
-            f"Причины:\n• " + "\n• ".join(result["reasons"])
-                 )
+        result = analyze_match(match)
+
+        if result["score"] >= 80:
+
+            text = (
+                "🟢 <b>STRONG SIGNAL</b>\n\n"
+                f"⚽ {match['home']} — {match['away']}\n"
+                f"🏆 {match['league']}\n\n"
+                f"📊 Оценка: {result['score']}/100\n\n"
+                "Причины:\n• "
+                + "\n• ".join(result["reasons"])
+            )
+
+            await send_message(text)
