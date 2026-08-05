d97ce8334c9173f1a5f773800538e365c259cc90
kosta7bravo-beep <kosta7bravo@gmail.com>
2026-08-05 21:04:20 +0300

    Update match_provider.py

--- match_provider.py
+++ match_provider.py
@@ -1,1 +1,30 @@
-
+def get_matches():
+    return [
+        {
+            "league": "Premier League",
+            "home": "Arsenal",
+            "away": "Chelsea",
+            "odd": 1.91,
+            "home_form": 4,
+            "away_form": 3,
+            "goals_avg": 2.9
+        },
+        {
+            "league": "La Liga",
+            "home": "Real Madrid",
+            "away": "Sevilla",
+            "odd": 1.83,
+            "home_form": 5,
+            "away_form": 2,
+            "goals_avg": 3.1
+        },
+        {
+            "league": "Serie A",
+            "home": "Inter",
+            "away": "Roma",
+            "odd": 2.15,
+            "home_form": 4,
+            "away_form": 4,
+            "goals_avg": 2.7
+        }
+    ]
