797bac0f04b86e0432adf280b061397f51dca6dc
kosta7bravo-beep <kosta7bravo@gmail.com>
2026-08-05 21:14:31 +0300

    Update football.py

--- football.py
+++ football.py
@@ -1,20 +1,21 @@
 from telegram_bot import send_message
 from match_analyzer import analyze_match
 from match_provider import get_matches
+from sent_storage import load_sent, save_sent
 
-already_sent = False
 
 async def check_football():
-    global already_sent
 
-    if already_sent:
-        return
-
-    already_sent = True
+    sent = load_sent()
 
     matches = get_matches()
 
     for match in matches:
+
+        match_id = f"{match['league']}_{match['home']}_{match['away']}"
+
+        if match_id in sent:
+            continue
 
         result = analyze_match(match)
 
@@ -22,11 +23,15 @@ async def check_football():
 
             text = (
                 "🟢 <b>STRONG SIGNAL</b>\n\n"
-                f"⚽ {match['home']} — {match['away']}\n"
-                f"🏆 {match['league']}\n\n"
+                f"🏆 {match['league']}\n"
+                f"⚽ {match['home']} — {match['away']}\n\n"
                 f"📊 Оценка: {result['score']}/100\n\n"
                 "Причины:\n• "
                 + "\n• ".join(result["reasons"])
             )
 
             await send_message(text)
+
+            sent.append(match_id)
+
+    save_sent(sent)
