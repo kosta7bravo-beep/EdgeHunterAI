2f33099871c050e1a5d247d74462c85a2554269c
kosta7bravo-beep <kosta7bravo@gmail.com>
2026-08-08 17:56:49 +0300

    Update football.py

--- football.py
+++ football.py
@@ -1,17 +1,1 @@
-        
-                    )
-                continue
 
-            text += format_bookmaker(
-                bookmaker_name,
-                markets
-            )
-
-            text += "\n"
-
-        # Пока просто показываем реальные данные.
-        # Прогнозы подключим после проверки.
-        if len(text) > 3800:
-            await send_message(text[:3800])
-        else:
-            await se
